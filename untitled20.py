b8c24617e30d2c1ecde202cb76e5eb76432040d9
Pavel <paulzinkevich1211@gmail.com>
2025-12-12 15:36:14 +0300

    Update untitled20.py

--- untitled20.py
+++ untitled20.py
@@ -270,6 +270,6 @@ with tabs[1]:
                     class_file = full_path.replace(".h5", "_classes.json")
                     if os.path.exists(class_file):
                         os.remove(class_file)
-                    st.success("Model is being deleted!")
+                    st.success("Model is being deleted, please refresh your page in a few seconds!")
                 except Exception as e:
                     st.error(f"Error deleting model: {e}")
